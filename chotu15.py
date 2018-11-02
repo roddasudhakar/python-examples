9dc074fecc132f265e8c9e1e11f5a09e7410775a
roddasudhakar <44562799+roddasudhakar@users.noreply.github.com>
2018-11-02 09:22:25 +0530

    Add files via upload

---
+++ chotu15.py
@@ -0,0 +1,14 @@
+name=input("enter coustomer name:")
+i=int(input("enter industry slab units:"))
+c=int(input("enter commercial slab units:"))
+r=int(input("enter residence slab units:"))
+a=i*5.25
+print("idustry total rate amount:",a)
+b=c*4.00
+print("commercial total rate amount:",b)
+d=r*3.08
+print("residence total rate amount:",d)
+total_amount=a+b+c
+print("total Coustemeramount:",total_amount)
+
+
