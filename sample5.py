4314a3ea930a852800d1791d2b49f5499158c030
roddasudhakar <44562799+roddasudhakar@users.noreply.github.com>
2018-12-02 02:47:12 +1200

    Add files via upload

---
+++ sample5.py
@@ -0,0 +1,9 @@
+from sample import employee,fun1,a,b
+f1=fun1()
+e1=employee()
+e1.companydetails()
+e1.assaign()
+e1.assaign(((input("enter idno:"))),input("enter employee name:,"),input("enter employee cnu:"))
+e1.emp_display()
+print(a)
+print(b)
